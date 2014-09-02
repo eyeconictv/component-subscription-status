0a020cb162d8ecfb141064cf6667dbcac953469c
Alex Deaconu <alex.deaconu@risevision.com>
2014-09-02 17:26:05 -0400

    [feature] Changed variables to use angular
    Code cleanup

--- dist/js/subscription-status.js
+++ dist/js/subscription-status.js
@@ -1,86 +1,102 @@
-/* global SUBSCRIPTION_STATUS_CONFIG: true */
-/* exported SUBSCRIPTION_STATUS_CONFIG */
-if (typeof SUBSCRIPTION_STATUS_CONFIG === "undefined") {
-  var SUBSCRIPTION_STATUS_CONFIG = {
-    STORE_URL: "http://store.risevision.com/~rvi/store/",
-    PRODUCT_PATH: "#/product/productId/",
-    STORE_SERVER_URL: "https://store-dot-rvaserver2.appspot.com/",
-    PATH_URL: "v1/company/companyId/product/status?pc="
-  };
-}
+(function () {
+  "use strict";
 
-"use strict";
+  angular.module("risevision.widget.common.subscription-status.config", [])
+    .value("STORE_URL", "http://store.risevision.com/~rvi/store/")
+    .value("PRODUCT_PATH", "#/product/productId/")
+    .value("STORE_SERVER_URL", "https://store-dot-rvaserver2.appspot.com/")
+    .value("PATH_URL", "v1/company/companyId/product/status?pc=")
+  ;
+}());
 
-angular.module("risevision.widget.common.subscription-status",
-  ["risevision.widget.common.service.store"])
-  .directive("subscriptionStatus", ["$templateCache", "storeService",
-    function ($templateCache, storeService) {
-    return {
-      restrict: "AE",
-      require: "?ngModel",
-      scope: {
-        productId: "@",
-        productCode: "@",
-        companyId: "@"
-      },
-      template: $templateCache.get("subscription-status-template.html"),
-      link: function($scope, elm, attrs, ctrl) {
-        if ($scope.productCode && $scope.companyId) {
-          storeService.getSubscriptionStatus($scope.productCode, $scope.companyId).then(function(subscriptionStatus) {
-            $scope.subscribed = false;
-            if (subscriptionStatus) {
-              $scope.subscribed = true;
-              $scope.subscriptionMessage = subscriptionStatus.status;
+(function () {
+  "use strict";
+
+  angular.module("risevision.widget.common.subscription-status",
+    ["risevision.widget.common.subscription-status.config",
+    "risevision.widget.common.service.store"])
+    .directive("subscriptionStatus", ["$templateCache", "storeService",
+      function ($templateCache, storeService) {
+      return {
+        restrict: "AE",
+        require: "?ngModel",
+        scope: {
+          productId: "@",
+          productCode: "@",
+          companyId: "@"
+        },
+        template: $templateCache.get("subscription-status-template.html"),
+        link: function($scope, elm, attrs, ctrl) {
+          $scope.$watch("companyId", function(companyId) {
+            if ($scope.productCode && $scope.productId && companyId) {
+              storeService.getSubscriptionStatus($scope.productCode, companyId).then(function(subscriptionStatus) {
+                $scope.subscribed = false;
+                if (subscriptionStatus) {
+                  $scope.subscribed = true;
+                  $scope.subscriptionMessage = subscriptionStatus.status;
+                }
+              });
             }
           });
+
+          if (ctrl) {
+            $scope.$watch("subscribed", function(subscribed) {
+              ctrl.$setViewValue(subscribed);
+            });
+          }
         }
+      };
+    }]);
+}());
 
-        if (ctrl) {
-          $scope.$watch("subscribed", function(subscribed) {
-            ctrl.$setViewValue(subscribed);
-          });
-        }
-      }
-    };
-  }]);
+(function () {
+  "use strict";
 
-angular.module("risevision.widget.common.subscription-status")
-  .filter("productUrl", function() {
-    return function(productId) {
-      if (productId) {
-        var url = SUBSCRIPTION_STATUS_CONFIG.STORE_URL;
-        url += SUBSCRIPTION_STATUS_CONFIG.PRODUCT_PATH.replace("productId", productId);
-        return url;
-      }
-      else {
-        return "";
-      }
-    };
-  });
-
-angular.module("risevision.widget.common.service.store", [])
-  .service("storeService", ["$http", "$q", function ($http, $q) {
-
-    this.getSubscriptionStatus = function (productCode, companyId) {
-      var deferred = $q.defer();
-
-      var url = SUBSCRIPTION_STATUS_CONFIG.STORE_SERVER_URL +
-        SUBSCRIPTION_STATUS_CONFIG.PATH_URL.replace("companyId", companyId) +
-        productCode;
-
-      $http.get(url).then(function (response) {
-        if (response && response.data && response.data.length) {
-          deferred.resolve(response.data[0]);
+  angular.module("risevision.widget.common.subscription-status")
+    .filter("productUrl", ["STORE_URL", "PRODUCT_PATH",
+    function(STORE_URL, PRODUCT_PATH) {
+      return function(productId) {
+        if (productId) {
+          var url = STORE_URL;
+          url += PRODUCT_PATH.replace("productId", productId);
+          return url;
         }
         else {
-          deferred.reject("No response");
+          return "";
         }
-      });
+      };
+    }]);
+}());
 
-      return deferred.promise;
-    };
+(function () {
+  "use strict";
 
-  }]);
+  angular.module("risevision.widget.common.service.store",
+    ["risevision.widget.common.subscription-status.config"])
+    .service("storeService", ["$http", "$q", "STORE_SERVER_URL", "PATH_URL",
+    function ($http, $q, STORE_SERVER_URL, PATH_URL) {
+
+      this.getSubscriptionStatus = function (productCode, companyId) {
+        var deferred = $q.defer();
+
+        var url = STORE_SERVER_URL +
+          PATH_URL.replace("companyId", companyId) +
+          productCode;
+
+        $http.get(url).then(function (response) {
+          if (response && response.data && response.data.length) {
+            deferred.resolve(response.data[0]);
+          }
+          else {
+            deferred.reject("No response");
+          }
+        });
+
+        return deferred.promise;
+      };
+
+    }]);
+}());
 
 (function(module) {
 try { app = angular.module("risevision.widget.common.subscription-status"); }
