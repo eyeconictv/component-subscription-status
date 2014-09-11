965b2ef408345088aadef0c2d0ec4a854240e9e6
Alex Deaconu <alex.deaconu@risevision.com>
2014-09-11 16:26:52 -0400

    [feature] Full-screen Store modal
    Created app.js file that initializes module
    Created directive that deals with opening Store modal
    Moved modal to be appended to the <body> element directly
    - This allows for a better full-screen experience
    - Styled modal similarly to the Widget Settings modal
    Removed unused css class

---
+++ src/app.js
@@ -0,0 +1,9 @@
+(function () {
+  "use strict";
+
+  angular.module("risevision.widget.common.subscription-status",
+    ["risevision.widget.common.subscription-status.config",
+    "risevision.widget.common.translate",
+    "risevision.widget.common.subscription-status.service",
+    "risevision.widget.common"]);
+  }());
