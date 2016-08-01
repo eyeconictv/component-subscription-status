664813834e343037e5df1930bbed0935671e1403
RV-Adrian <adrian.dalimonte@risevision.com>
2016-08-01 11:53:21 -0400

    Subscription links open new Tab
    Cleaned up IFrame code

--- test/unit/dtv-subscription-status-spec.js
+++ test/unit/dtv-subscription-status-spec.js
@@ -7,26 +7,6 @@ describe("directive: subscription status", function() {
 
   beforeEach(module("risevision.widget.common.subscription-status"));
   beforeEach(module(function ($provide) {
-    $provide.service("$modal", function() {
-      return {
-        open: function() {
-          var deferred = Q.defer();
-          
-          if (modalSuccess) {
-            modalResult = true;
-            deferred.resolve();
-          }
-          else {
-            modalResult = false;
-            deferred.reject();
-          }
-
-          return {
-            result: deferred.promise
-          };
-        }
-      };
-    });
     $provide.service("subscriptionStatusService", function() {
       return {
         get: function() {
@@ -70,14 +50,15 @@ describe("directive: subscription status", function() {
   });
   
   it("should add watchers", function() {
-    expect(element.isolateScope().$$watchers[0].exp).to.equal("showStoreModal");
-    expect(element.isolateScope().$$watchers[1].exp).to.equal("subscriptionStatus");
-    expect(element.isolateScope().$$watchers[2].exp).to.equal("companyId");
+    expect(element.isolateScope().$$watchers[0].exp).to.equal("subscriptionStatus");
+    expect(element.isolateScope().$$watchers[1].exp).to.equal("companyId");
+    expect(element.isolateScope().$$watchers[2]).to.be.undefined;
   });
   
   it("watch companyId and update checkSubscriptionStatus", function() {
     expect(element.isolateScope().subscriptionStatus).to.deep.equal({"status": "trial"});
-    expect(element.isolateScope().storeAccountUrl).to.equal("https://store.risevision.com//account?cid=companyId");
+    expect(element.isolateScope().storeAccountUrl).to.equal("https://store.risevision.com/account?cid=companyId");
+    expect(element.isolateScope().storeUrl).to.equal("https://store.risevision.com/product/productId/?cid=companyId");
 
     $emitSpy.should.have.been.calledOnce;
     $emitSpy.should.have.been.calledWith("subscription-status:changed", {status: "trial"});
@@ -101,46 +82,4 @@ describe("directive: subscription status", function() {
     }, 10);
   });
 
-  describe("showStoreModal:", function() {
-    it("should close modal", function(done) {
-      element.isolateScope().showStoreModal = true;
-      $rootScope.$digest();
-
-      setTimeout(function() {
-        expect(modalResult).to.be.true;
-        
-        done();
-      }, 10);
-    });
-
-    it("should dismiss modal", function(done) {
-      modalSuccess = false;
-
-      element.isolateScope().showStoreModal = true;
-      $rootScope.$digest();
-
-      setTimeout(function() {
-        expect(modalResult).to.be.false;
-        
-        done();
-      }, 10);
-    });
-
-    it("should emit event when subscription status changes", function(done) {
-      $emitSpy.reset();
-
-      subscriptionSuccess = true;
-      element.isolateScope().showStoreModal = true;
-      $rootScope.$digest();
-
-      setTimeout(function() {
-        $emitSpy.should.have.been.calledOnce;
-        $emitSpy.should.have.been.calledWith("subscription-status:changed", {status: "subscribed"});
-
-        done();
-      }, 10);
-    });
-
-  });
-  
 });
